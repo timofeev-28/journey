36fb1b00e07f27e48c46ea02f48dae5f63c16e93
Dmitrii Timofeev <28dngn@gmail.ru>
2023-12-04 18:21:41 +0300

    добавлены фавиконки и переименованы изобр для постера

--- gulp/copyAssets.mjs
+++ gulp/copyAssets.mjs
@@ -14,7 +14,8 @@ const copy = () =>
     'source/fonts/**',
     'source/img/**',
     'source/favicon/**',
-    'source/**.ico'
+    'source/**.ico',
+    'source/*.webmanifest'
   ], {
     base: 'source',
   })
